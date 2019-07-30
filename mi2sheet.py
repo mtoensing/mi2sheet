1fdaf766820582951e08ca8550c38afcdf9b19b4
Marc Tönsing <marc@marc.tv>
2019-07-30 22:56:24 +0200

    Added support for mysql and weather API

--- mi2sheet.py
+++ mi2sheet.py
@@ -7,18 +7,68 @@ import logging
 import sys
 import gspread
 import datetime
+import pymysql
+import json
+import urllib.request
 
 from btlewrap import available_backends, BluepyBackend, GatttoolBackend, PygattBackend
 from mitemp_bt.mitemp_bt_poller import MiTempBtPoller, \
     MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
 from oauth2client.service_account import ServiceAccountCredentials
 
-def valid_mitemp_mac(mac, pat=re.compile(r"4C:65:A8:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}")):
-    """Check for valid mac adresses."""
-    if not pat.match(mac.upper()):
-        raise argparse.ArgumentTypeError('The MAC address "{}" seems to be in the wrong format'.format(mac))
-    return mac
+def pollweather(args):
 
+    apikey = args.apikey
+    city = args.city
+    # Note f before first quote of string
+    apiurl = f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={apikey}&units=metric"
+
+    req = urllib.request.Request(apiurl)
+    r = urllib.request.urlopen(req).read()
+    jsonreponse = json.loads(r.decode('utf-8'))
+
+    temperature = jsonreponse['main']['temp']
+    humidity = jsonreponse['main']['humidity']
+    wind = jsonreponse['wind']['speed']
+
+    data = [temperature,humidity,wind]
+
+    return data
+
+def writeweather(args):
+
+    row = pollweather(args)
+
+    temperature = row[0];
+    humidity = row[1];
+    wind = row[2];
+    city = args.city
+
+    writeMySQL(args, city , None, 'temperature', temperature, None , None )
+    writeMySQL(args, city , None, 'humidity', humidity, None , None )
+    writeMySQL(args, city , None, 'wind', wind, None , None )
+
+
+def pollData(args):
+    """Poll data from the sensor."""
+    backend = _get_backend(args)
+    poller = MiTempBtPoller(args.mac, backend)
+
+    #print("Getting data from Mi Temperature and Humidity Sensor")
+    #print("FW: {}".format(poller.firmware_version()))
+    #print("Name: {}".format(poller.name()))
+    #print("Battery: {}".format(poller.parameter_value(MI_BATTERY)))
+    #print("Temperature: {}".format(poller.parameter_value(MI_TEMPERATURE)))
+    #print("Humidity: {}".format(poller.parameter_value(MI_HUMIDITY)))
+
+    name = format(poller.name())
+    battery = format(poller.parameter_value(MI_BATTERY))
+    temperature = format(poller.parameter_value(MI_TEMPERATURE))
+    humidity = format(poller.parameter_value(MI_HUMIDITY))
+
+    data = [temperature,humidity,battery]
+    print(data)
+    return data
 
 def poll(args):
     """Poll data from the sensor."""
@@ -60,6 +110,38 @@ def pollandwrite(args):
     #print("Writing to sheet")
     sheet.insert_row(row, rowindex, value_input_option='USER_ENTERED')
 
+def pollandwrite_mysql(args):
+
+    row = pollData(args);
+
+    temperature = row[0];
+    humidity = row[1];
+    battery = row[2];
+    device = args.device
+
+    writeMySQL(args, device, None, 'temperature', temperature, None , None )
+    writeMySQL(args, device, None, 'humidity', humidity, None , None )
+    writeMySQL(args, device, None, 'battery', battery, None , None )
+
+def writeMySQL(args,device ,type ,event ,value ,reading ,unit):
+
+    db = pymysql.connect(args.server,args.user,args.password,args.db )
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Prepare SQL query to INSERT a record into the database.
+    sql = "INSERT INTO history (TIMESTAMP,DEVICE,TYPE,EVENT,VALUE,READING,UNIT) VALUES (NOW(), %s, %s, %s, %s,%s ,%s)"
+    val = (device ,type ,event ,value ,reading ,unit)
+
+    try:
+       # Execute the SQL command
+       cursor.execute(sql, val)
+    except:
+       print ("Error: unable write")
+
+    # disconnect from server
+    db.close()
 
 def _get_backend(args):
     """Extract the backend class from the command line arguments."""
@@ -91,16 +173,34 @@ def main():
     subparsers = parser.add_subparsers(help='sub-command help', )
 
     parser_poll = subparsers.add_parser('poll', help='poll data from a sensor')
-    parser_poll.add_argument('mac', type=valid_mitemp_mac)
+    parser_poll.add_argument('mac', type=str)
     parser_poll.set_defaults(func=poll)
 
     parser_poll = subparsers.add_parser('pollandwrite', help='poll and write data from a sensor to spreadsheet')
-    parser_poll.add_argument('mac', type=valid_mitemp_mac)
+    parser_poll.add_argument('mac', type=str)
     parser_poll.add_argument('keyfile', type=str)
     parser_poll.add_argument('sheetname', type=str)
     parser_poll.add_argument('worksheet', type=int, default=0)
     parser_poll.add_argument('rowindex', type=int, default=2)
     parser_poll.set_defaults(func=pollandwrite)
+
+    parser_poll = subparsers.add_parser('pollandwrite_mysql', help='poll and write data from a sensor to mysql')
+    parser_poll.add_argument('mac', type=str)
+    parser_poll.add_argument('server', type=str)
+    parser_poll.add_argument('user', type=str)
+    parser_poll.add_argument('password', type=str)
+    parser_poll.add_argument('db', type=str)
+    parser_poll.add_argument('device', type=str)
+    parser_poll.set_defaults(func=pollandwrite_mysql)
+
+    parser_poll = subparsers.add_parser('writeweather', help='poll and return weather')
+    parser_poll.add_argument('server', type=str)
+    parser_poll.add_argument('user', type=str)
+    parser_poll.add_argument('password', type=str)
+    parser_poll.add_argument('db', type=str)
+    parser_poll.add_argument('apikey', type=str)
+    parser_poll.add_argument('city', type=str)
+    parser_poll.set_defaults(func=writeweather)
 
     parser_scan = subparsers.add_parser('backends', help='list the available backends')
     parser_scan.set_defaults(func=list_backends)
---
+++ writesensor_mysql.sh
@@ -0,0 +1,1 @@
+python3 /home/pi/mi2sheet/mi2sheet.py --backend bluepy pollandwrite_mysql XX:XX:XX:XX:XX:XX "server.to" "user" "secret" "db" "devicename"
