9b677e7461ce06f17b41a6285660cec67f67e565
Marc Tönsing <marc@marc.tv>
2019-07-31 07:36:44 +0200

    ups

--- writeweather.sh
+++ writeweather.sh
@@ -1,1 +1,1 @@
-python3 /home/pi/mi2sheet/mi2sheet.py --backend bluepy writeweather "marc.tv" "d02f2f57" "xrhMjgKR3nr" "d02f2f57" "e91f03bee4b45ab0bfb5d64ec04bc5c9" "Hannover,de"
+python3 /home/pi/mi2sheet/mi2sheet.py --backend bluepy writeweather "user" "secret" "db" "devicename" "weatherapikey" "Hannover,de"
